95c4947a908e628afc266cedcba1e5535bf54e07
OleksiiDubovyk <66566223+OleksiiDubovyk@users.noreply.github.com>
2024-12-13 04:33:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const countDownDateTime = new Date("2024-01-12T12:17:20+01:00").getTime();
+const countDownDateTime = new Date("2025-01-24T11:00:00+01:00").getTime();
 const daysValue = document.querySelector("#days");
 const hoursValue = document.querySelector("#hours");
 const minutesValue = document.querySelector("#minutes");
